f6741fb82468a18435630a4d7b65630ef586a696
takazo3 <info@re-zero-jp.com>
2022-03-18 12:35:28 +0900

    サーチ機能、エラーの解消。使用方法と免責に関するドキュメント

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
-
+import json
+import os
+import sys
 
 FONT_NAME = "Arial"
 BLUE = "#4FD3C4"
 PINK = "#e2979c"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
 
     password_letters = [choice(letters) for _ in range(randint(8,10))]
     password_symbols = [choice(symbols) for _ in range(randint(2,4))]
@@ -32,22 +34,50 @@ def generate_password():
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
-    email =email_entry.get()
-    password =password_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
-    if len(email)==0 or len(password)==0:
-        messagebox.showinfo(title="入力エラー",message="入力されていない項目があります。")
+    if len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="入力エラー", message="入力されていない項目があります。")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"内容: \nEmail:{email}\nPassword: {password}\nこれを記録しますか？")
+        try:
+            with open("data.json", "r") as data_file:
+                data =json.load(data_file)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
 
-        if is_ok:
-            with  open("data.txt","a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+#----------------------------FIND FUNCTION__==__________________________#
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="エラー", message="ファイルがありません")
 
-
+    else:
+            if website in data:
+                email = data[website]["email"]
+                password =data[website]["password"]
+                messagebox.showinfo(title=website, message=f"Email:{email}\nPassword:{password}")
+                pyperclip.copy(password)
+            else:
+                messagebox.showinfo(title="エラー", message=f"{website}のデータはありません")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -56,7 +86,7 @@ window.config(padx=50,pady=50)
 
 
 canvas = Canvas(width=200,height=200)
-main_image = PhotoImage(file='logo.png')
+main_image = PhotoImage(file=os.path.join(sys._MEIPASS, 'logo.png'))
 canvas.create_image(100,100, image=main_image)
 canvas.grid(column=1, row=0)
 
@@ -85,20 +115,13 @@ password_entry.grid(column=1,row=3)
 
 
 #Button
-generate_button =Button(text="Generate Password", bg=BLUE,command=generate_password)
+search_button = Button(text="Search", bg="#332FD0",width=15, command=find_password)
+search_button.grid(column=2,row=1)
+
+generate_button =Button(text="パスワード自動生成", bg=BLUE,command=generate_password)
 generate_button.grid(column=2,row=3)
 
-add_button =Button(text="Save Password", width=47, bg=PINK, command=save)
+add_button =Button(text="パスワードを保存する", width=47, bg=PINK, command=save)
 add_button.grid(column=1, row=4,columnspan=2)
-
-
-
-
-
-
-
-
-
-
 
 window.mainloop()
